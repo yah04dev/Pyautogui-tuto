7bae084739eea6e3229d7ac4354acaf256403df7
yah04dev <46074913+yah04dev@users.noreply.github.com>
2019-12-07 13:41:13 +0100

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -1,7 +1,7 @@
 import pyautogui
-
-position =pyautogui.position()gimp
+position =pyautogui.position()#entre your favorite position (x,y)
 pyautogui.click(position)
-pyautogui.typewrite("gimp")
-pyautogui.press("enter")
+pyautogui.typewrite("gimp")#write you favorite software name
+pyautogui.press("enter")# this line mean the button that the proggramme press here ist enter 
+#you can know your mouse position when you run your_mouse_position.py
 
