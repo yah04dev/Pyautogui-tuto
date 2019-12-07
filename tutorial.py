2025e12faca3a056855f9fa4d5eca60352129a92
yah04dev <46074913+yah04dev@users.noreply.github.com>
2019-12-07 13:29:31 +0100

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -1,7 +1,6 @@
 import pyautogui
-
-position =pyautogui.position()gimp
+position =pyautogui.position(0,899)//entre your own mouse position
 pyautogui.click(position)
-pyautogui.typewrite("gimp")
-pyautogui.press("enter")
+pyautogui.typewrite("gimp")//entre your software name exemple
+pyautogui.press("enter")//this mean press entre to entre to the software
 
